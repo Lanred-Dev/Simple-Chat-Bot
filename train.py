28e10505788f437758bd49aabda2d545bb945a7a
Landon Redmond <landon.redmond0@gmail.com>
2025-05-02 01:23:31 -0500

    Improve train mode

--- train.py
+++ train.py
@@ -1,18 +1,36 @@
 import json
 
-def addTrainingData(input, response):
+def add_sample(message: str, response: str):
     with open('samples.json') as file:
-        samples = json.load(file)
+        SAMPLES = json.load(file)
 
-    samples.append({
-        'input': input,
-        'response': response
+    SAMPLES.append({
+        "input": message,
+        "response": response
     })
 
     with open('samples.json', 'w') as file:
-        json.dump(samples, file, indent=4)
+        json.dump(SAMPLES, file, indent=4)
 
-while True:
-    userInput = input('Input: ')
-    response = input('Response: ')
-    addTrainingData(userInput, response)
+def prompt_mode():
+    print("""
+    # (1) 'train' - Train the bot with samples\n
+    # (2) 'quit' - Exit the program
+    """)
+
+    mode = input("What mode do you want to enter? (train/quit): ")
+
+    print(f"{mode} mode selected.\n")
+
+    if mode == 'train':
+        print("Samples are formatted as: \nInput: <user input> \nResponse: <bot response>\n")
+
+        while True:
+            message = input('Input: ')
+            response = input('Response: ')
+            add_sample(message, response)
+    elif mode == 'quit':
+        print("Exiting the program.")
+        exit()
+
+prompt_mode()
